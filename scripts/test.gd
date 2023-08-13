8afa2fd11e71f103a168363e046a6111cb5c6150
dragontheminerz45 <138758831+baihaqibb@users.noreply.github.com>
2023-08-13 17:39:00 +0700

    Main Menu (Improvisation #2)
    Added pausing and pause menu
    
    (bg art and styling STILL pending)

--- scripts/test.gd
+++ scripts/test.gd
@@ -1,21 +1,18 @@
-extends Control
+extends Node
 
-@onready var sfx = $SFX
-@onready var texture_rect = $CanvasLayer/TextureRect
+@onready var sfx = $UI/SFX
+@onready var pause_screen = $UI/CanvasLayer/PauseScreen
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	get_tree().paused = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# ~~~DEBUG~~~
-	# ESC to exit test scene and back to main menu
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-	# ENTER to play vine boom sound effect cuz i hate myself
-	if Input.is_action_just_pressed("ui_accept"):
-		texture_rect.scale *= 1.042069
-		sfx.play()
-	# spin, not really necessary
-	texture_rect.rotation += 2.1 * delta
+	if not get_tree().paused:
+		if Input.is_action_just_pressed("ui_cancel"):
+			pause_screen.visible = true
+			get_tree().paused = true
+		if Input.is_action_just_pressed("ui_accept"):
+			sfx.play()
---
+++ scripts/test_level.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var sprite_2d = $Sprite2D
+@onready var sfx = $SFX
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		sprite_2d.scale *= 1.042069
+	sprite_2d.rotation += 2.1 * delta
