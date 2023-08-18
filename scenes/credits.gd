b3c0c1de6f1b5d834846dc83df8b653e866df566
dragontheminerz45 <138758831+baihaqibb@users.noreply.github.com>
2023-08-18 22:33:13 +0700

    Added Credits
    Still WIP (takutnya ada lagi yg mau ditambahin di credits)

--- scenes/credits.gd
+++ scenes/credits.gd
@@ -1,13 +1,14 @@
-extends Node2D
+extends Panel
 
-@onready var sprite_2d = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		sprite_2d.scale *= 1.042069
-	sprite_2d.rotation += 2.1 * delta
+	pass
+
+func _on_close_button_pressed():
+	self.visible = false
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var title_menu = $CanvasLayer/TitleMenu
 @onready var settings_menu = $CanvasLayer/SettingsMenu
+@onready var credits = $CanvasLayer/Credits
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,13 +10,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	title_menu.visible = not settings_menu.visible
+	title_menu.visible = not (settings_menu.visible or credits.visible)
 
 func _on_play_game_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/test.tscn")
 
 func _on_settings_button_pressed():
 	settings_menu.visible = true
+
+func _on_credits_button_pressed():
+	credits.visible = true
 
 func _on_quit_button_pressed():
 	get_tree().quit()
